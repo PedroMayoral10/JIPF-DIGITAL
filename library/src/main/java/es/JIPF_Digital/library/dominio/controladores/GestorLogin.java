9562ce90d1291f48b7df0b70e6bbd69c466cb2d1
FranEspada13 <FcoJavier.Espada@alu.uclm.es>
2024-10-22 13:00:11 +0200

    Acceso de menu principal a pedidos, preparada pedidos.html

--- library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorLogin.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorLogin.java
@@ -11,16 +11,24 @@ import es.JIPF_Digital.library.dominio.entidades.Usuario;
 
 @Controller
 public class GestorLogin {
-
+	
 	@GetMapping("/login")
 		public String loginForm(Model model) {
 			model.addAttribute("usuario", new Usuario());
 			return "login";
 	}
 	
+	@GetMapping("/menuprincipal")
+		public String menu() {
+		
+			return "menuprincipal";
+		}
+	
 	@PostMapping("/login")
 	public String loginSubmit(@ModelAttribute Usuario usuario, Model model) {
 		model.addAttribute("usuario", usuario);
 		return "menuprincipal";
 	}
+	
+	
 }
--- library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorPedidos.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorPedidos.java
@@ -3,65 +3,58 @@ package es.JIPF_Digital.library.dominio.controladores;
 import es.JIPF_Digital.library.persistencia.*;
 import es.JIPF_Digital.library.dominio.entidades.*;
 import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+
+@Controller
 public class GestorPedidos {
 
 	PedidoDAO pedidoDAO;
 	ServicioEntregaDAO servicioEntregaDAO;
 	Pedido pedidoEnMarcha;
 
-	/**
-	 * 
-	 * @param c
-	 * @param r
-	 * @param items
-	 */
+
+	@GetMapping("/pedidos")
+	public String pedidosForm(Model model) {
+		return "pedidos";
+	}
+
+	
+	
 	public void realizarPedido(Cliente c, Restaurante r, List<ItemMenu> items) {
 		// TODO - implement GestorPedidos.realizarPedido
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param p
-	 */
+
 	private boolean realizarPago(Pedido p) {
 		// TODO - implement GestorPedidos.realizarPago
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param p
-	 * @param d
-	 */
+
 	private ServicioEntrega crearServicioEntrega(Pedido p, Direccion d) {
 		// TODO - implement GestorPedidos.crearServicioEntrega
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param item
-	 */
+
 	public void addItemMenu(ItemMenu item) {
 		// TODO - implement GestorPedidos.addItemMenu
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param item
-	 */
+
 	public void eliminarItemMenu(ItemMenu item) {
 		// TODO - implement GestorPedidos.eliminarItemMenu
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param resaturante
-	 */
+
 	public void comenzarPedido(Restaurante resaturante) {
 		// TODO - implement GestorPedidos.comenzarPedido
 		throw new UnsupportedOperationException();
