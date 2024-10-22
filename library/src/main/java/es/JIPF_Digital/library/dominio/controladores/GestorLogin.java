d6c770a5d1deb37a47fe0552534a1789108d7837
FranEspada13 <FcoJavier.Espada@alu.uclm.es>
2024-10-22 12:08:14 +0200

    Login y menu principal hechos

--- library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorLogin.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorLogin.java
@@ -1,15 +1,26 @@
 package es.JIPF_Digital.library.dominio.controladores;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import es.JIPF_Digital.library.dominio.entidades.Usuario;
+
+@Controller
 public class GestorLogin {
 
-	/**
-	 * 
-	 * @param id
-	 * @param pass
-	 */
-	public void login(String id, String pass) {
-		// TODO - implement GestorLogin.login
-		throw new UnsupportedOperationException();
+	@GetMapping("/login")
+		public String loginForm(Model model) {
+			model.addAttribute("usuario", new Usuario());
+			return "login";
 	}
-
+	
+	@PostMapping("/login")
+	public String loginSubmit(@ModelAttribute Usuario usuario, Model model) {
+		model.addAttribute("usuario", usuario);
+		return "menuprincipal";
+	}
 }
--- library/src/main/java/es/JIPF_Digital/library/dominio/entidades/Usuario.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/entidades/Usuario.java
@@ -3,7 +3,40 @@ package es.JIPF_Digital.library.dominio.entidades;
 public class Usuario {
 
 	private String idUsuario;
+	private String nombre;
 	private String pass;
 	private int attribute;
+	
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
+	
+	public int getAttribute() {
+		return attribute;
+	}
+	
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	public void setAtribute(int attribute) {
+		this.attribute = attribute;
+	}
 
 }
