e3bd5aad53c21b829a39febcbb88e39de937367e
FranEspada13 <FcoJavier.Espada@alu.uclm.es>
2024-11-02 21:27:49 +0100

    Terminada rama de realizar pedido

--- library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorPedidos.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorPedidos.java
@@ -14,24 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import es.JIPF_Digital.library.dominio.entidades.CartaMenu;
-import es.JIPF_Digital.library.dominio.entidades.Cliente;
-import es.JIPF_Digital.library.dominio.entidades.Direccion;
-import es.JIPF_Digital.library.dominio.entidades.EstadoPedido;
-import es.JIPF_Digital.library.dominio.entidades.ItemMenu;
-import es.JIPF_Digital.library.dominio.entidades.MetodoPago;
-import es.JIPF_Digital.library.dominio.entidades.Pago;
-import es.JIPF_Digital.library.dominio.entidades.Pedido;
-import es.JIPF_Digital.library.dominio.entidades.Repartidor;
-import es.JIPF_Digital.library.dominio.entidades.Restaurante;
-import es.JIPF_Digital.library.dominio.entidades.ServicioEntrega;
-import es.JIPF_Digital.library.persistencia.CartaMenuDAO;
-import es.JIPF_Digital.library.persistencia.ClienteDAO;
-import es.JIPF_Digital.library.persistencia.DireccionDAO;
-import es.JIPF_Digital.library.persistencia.ItemMenuDAO;
-import es.JIPF_Digital.library.persistencia.PedidoDAO;
-import es.JIPF_Digital.library.persistencia.RepartidorDAO;
-import es.JIPF_Digital.library.persistencia.RestauranteDAO;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import es.JIPF_Digital.library.dominio.entidades.*;
+import es.JIPF_Digital.library.persistencia.*;
 
 @Controller
 public class GestorPedidos {
@@ -50,17 +37,17 @@ public class GestorPedidos {
 	private RepartidorDAO repartidorDAO;
 	@Autowired
 	private DireccionDAO direccionDAO;
-
-	@GetMapping("/pedidos")
-	public String pedidosForm(Model model) {
-		return "pedidos";
-	}
-
-	// Obtenemos los ID's de los items para luego usarlos en el POSTmapping
+	
+	
 	private static List<Long> itemIds = new ArrayList<>();
 
 	private static List<ItemMenu> itemsPedidos = new ArrayList<>();
-
+	
+	/*
+	 * GETMAPPINGS------------------------------------
+	 * ---------------------------------------------
+	 */
+	
 	@GetMapping("/realizarpedido/{id_cliente}/{id_restaurante}")
 	public String detalleRestaurante(@PathVariable("id_cliente") String idCliente,
 			@PathVariable("id_restaurante") String idRestaurante, Model model) {
@@ -71,7 +58,56 @@ public class GestorPedidos {
 		model.addAttribute("cliente", cliente);
 		return "realizarpedido";
 	}
+	
+	
+	@GetMapping("/realizarpago/{idCliente}/{idRestaurante}")
+	public String realizarPago(@PathVariable("idCliente") String idCliente,
+			@PathVariable("idRestaurante") String idRestaurante, @RequestParam Map<String, String> params,
+			Model model) {
 
+		itemsPedidos = obtenerItems(params);
+
+		for (ItemMenu item : itemsPedidos) {
+			itemIds.add(item.getId()); // Suponiendo que hay un método getId() en ItemMenu
+		}
+
+		double precioTotalPedido = 0.0;
+		for (ItemMenu item : itemsPedidos) {
+			precioTotalPedido += item.getPrecio();
+		}
+		BigDecimal precioTotalRedondeado = BigDecimal.valueOf(precioTotalPedido).setScale(2, RoundingMode.HALF_UP);
+	    double precioTotal = precioTotalRedondeado.doubleValue();
+
+		// Añadir los ítems al modelo junto con los identificadores de cliente y
+		// restaurante
+		model.addAttribute("itemsPedidos", itemsPedidos);
+		model.addAttribute("idCliente", idCliente);
+		model.addAttribute("idRestaurante", idRestaurante);
+		model.addAttribute("precioTotal", precioTotal);
+
+		return "realizarpago";
+	}
+	
+	@GetMapping("/confirmacionpago/{idCliente}")
+	public String confirmacionpago(@PathVariable("idCliente") String idCliente, Model model) {
+		model.addAttribute("idCliente", idCliente); 
+		return "confirmacionPago";
+	}
+	
+	@GetMapping("/pedidoscliente/{idCliente}")
+	public String mostrarPedidos(@PathVariable("idCliente") String idCliente, Model model) {
+		List <Pedido> pedidos = pedidoDAO.findPedidosByCliente(idCliente);
+		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("idCliente", idCliente);
+		
+		return "pedidoscliente";
+	}
+ 
+	/*
+	 * POSTMAPPINGS------------------------------------
+	 * ---------------------------------------------
+	 */
+	
 	@PostMapping("/realizarpedido/{id_cliente}/{id_restaurante}")
 	public String procesarPedido(@PathVariable("id_cliente") String idCliente,
 			@PathVariable("id_restaurante") String idRestaurante, Model model,
@@ -88,50 +124,20 @@ public class GestorPedidos {
 		return "redirect:/realizarpedido/" + idCliente + "/" + idRestaurante;
 	}
 
-	@GetMapping("/realizarpago/{idCliente}/{idRestaurante}")
-	public String realizarPago(@PathVariable("idCliente") String idCliente,
-			@PathVariable("idRestaurante") String idRestaurante, @RequestParam Map<String, String> params,
-			Model model) {
-
-		itemsPedidos = obtenerItems(params);
-
-		for (ItemMenu item : itemsPedidos) {
-			itemIds.add(item.getId()); // Suponiendo que hay un método getId() en ItemMenu
-		}
-
-		double precioTotalPedido = 0.0;
-		for (ItemMenu item : itemsPedidos) {
-			precioTotalPedido += item.getPrecio();
-		}
-
-		// Añadir los ítems al modelo junto con los identificadores de cliente y
-		// restaurante
-		model.addAttribute("itemsPedidos", itemsPedidos);
-		model.addAttribute("idCliente", idCliente);
-		model.addAttribute("idRestaurante", idRestaurante);
-		model.addAttribute("precioTotal", precioTotalPedido);
-
-		return "realizarpago";
-	}
- 
-	@GetMapping("/confirmacionpago/{idCliente}")
-	public String confirmacionpago(@PathVariable("idCliente") String idCliente, Model model) {
-		model.addAttribute("idCliente", idCliente); 
-		return "confirmacionPago";
-	}
+	
+	
 	@PostMapping("/realizarpago/{idCliente}/{idRestaurante}")
 	public String submitPago(@PathVariable("idCliente") String idCliente,
 			@PathVariable("idRestaurante") String idRestaurante, Model model,
 			@RequestParam(value = "codigoPostal", required = false) String codigoPostal,
 			@RequestParam("metodoPago") MetodoPago tipo,
-			@RequestParam("fechaTransaccion") String fechaTransaccionString,
 			@RequestParam(value = "calle", required = false) String calle,
 			@RequestParam(value = "numero", required = false) String numero,
 			@RequestParam(value = "complemento", required = false) String complemento,
 			@RequestParam(value = "municipio", required = false) String municipio) {
 
 		// Convertir la fecha de transacción de String a LocalDate
-		LocalDate fechaTransaccion = LocalDate.parse(fechaTransaccionString);
+		LocalDate fechaTransaccion = LocalDate.now();
 
 		Restaurante restaurante = restauranteDAO.findById(idRestaurante).orElse(null);
 		Cliente cliente = clienteDAO.findById(idCliente).orElse(null);
@@ -169,10 +175,7 @@ public class GestorPedidos {
 			ItemMenu item = itemMenuDAO.findById(id_item).orElse(null);
 			itemsPedidos.add(item);
 			index++;
-			System.out.println(item.getNombre());
 		}
-
-		System.out.println(itemsPedidos.size());
 
 		return itemsPedidos;
 
--- library/src/main/java/es/JIPF_Digital/library/persistencia/PedidoDAO.java
+++ library/src/main/java/es/JIPF_Digital/library/persistencia/PedidoDAO.java
@@ -2,10 +2,14 @@ package es.JIPF_Digital.library.persistencia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 import es.JIPF_Digital.library.dominio.entidades.Pedido;
 
 @Repository
 public interface PedidoDAO extends JpaRepository<Pedido, Long> {
-   
+	@Query("SELECT p FROM Pedido p WHERE p.cliente.idUsuario = :idCliente")
+    List<Pedido> findPedidosByCliente(@Param("idCliente") String idCliente);
 }
