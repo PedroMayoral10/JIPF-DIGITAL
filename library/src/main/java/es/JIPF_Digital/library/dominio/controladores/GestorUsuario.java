7cca5e9587f0e2a9b68e61042f63cdde8a09334f
ivanromorg <Ivan.Romo@alu.uclm.es>
2024-11-25 17:16:36 +0100

    Arreglo de issues

--- library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorUsuario.java
+++ library/src/main/java/es/JIPF_Digital/library/dominio/controladores/GestorUsuario.java
@@ -106,16 +106,20 @@ public class GestorUsuario {
 			System.out.println(rol);
 			switch(rol) {
 			case 1:
-				Cliente cliente = new Cliente(usuario.getIdUsuario(), usuario.getNombre(), usuario.getPass(), apellidosCliente, dniCliente);
+				Cliente cliente = new Cliente(usuario.getIdUsuario(), usuario.getNombre(), usuario.getPass(),
+				 apellidosCliente, dniCliente);
 				clienteDAO.save(cliente);
 				break;
 			case 2:
-				Direccion dir = new Direccion(codigoPostalRestaurante, calleRestaurante, numeroRestaurante, complementoRestaurante, municipioRestaurante);
-				Restaurante restaurante = new Restaurante(usuario.getIdUsuario(), usuario.getNombre(), usuario.getPass(), dir, cifRestaurante);
+				Direccion dir = new Direccion(codigoPostalRestaurante, calleRestaurante, numeroRestaurante, complementoRestaurante,
+				 municipioRestaurante);
+				Restaurante restaurante = new Restaurante(usuario.getIdUsuario(), usuario.getNombre(),
+				 usuario.getPass(), dir, cifRestaurante);
 				restauranteDAO.save(restaurante);
 				break;
 			case 3:
-				Repartidor repartidor = new Repartidor(usuario.getIdUsuario(), usuario.getNombre(), usuario.getPass(), apellidosRepartidor, nifRepartidor);
+				Repartidor repartidor = new Repartidor(usuario.getIdUsuario(), usuario.getNombre(), 
+				usuario.getPass(),apellidosRepartidor, nifRepartidor);
 				repartidorDAO.save(repartidor);
 				break;
 			}
