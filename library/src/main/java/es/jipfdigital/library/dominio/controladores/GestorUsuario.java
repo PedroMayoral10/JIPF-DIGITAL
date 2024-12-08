53a43bf730a4ee0f6c51de659d47ec3475061417
FranEspada13 <FcoJavier.Espada@alu.uclm.es>
2024-12-08 12:10:09 +0100

    solucionados issues GestorUsuario

--- library/src/main/java/es/jipfdigital/library/dominio/controladores/GestorUsuario.java
+++ library/src/main/java/es/jipfdigital/library/dominio/controladores/GestorUsuario.java
@@ -27,13 +27,13 @@ public class GestorUsuario {
 	private static final String ERROR_STR = "error";
 	private static final String CONTRASENA_STR = "Contraseña incorrecta, pruebe otra vez";
 	private static final String RELLENA_CAMPOS = "Rellena todos los campos";
+	private static final String USUARIO_EXISTE_STR = "El nombre de usuario ya existe";
 	@Autowired
 	private ClienteDAO clienteDAO;
 	@Autowired
 	private RestauranteDAO restauranteDAO;
 	@Autowired
 	private RepartidorDAO repartidorDAO;
-	
 
 	/*
 	 * GETMAPPINGS
@@ -59,33 +59,33 @@ public class GestorUsuario {
 	/*
 	 * POSTMAPPINGS
 	 */
-	
-	@PostMapping("/login")	
+
+	@PostMapping("/login")
 	public String loginSubmit(Usuario usuario, Model model) {
-	    if (usuario == null) {
-	        model.addAttribute(ERROR_STR, "El usuario no existe, pruebe otra vez");
-	        return LOGIN_STR;
-	    }
+		if (usuario == null) {
+			model.addAttribute(ERROR_STR, "El usuario no existe, pruebe otra vez");
+			return LOGIN_STR;
+		}
 
-	    model.addAttribute(USUARIO_STR, usuario);
-	    Optional<Cliente> clienteOpt = clienteDAO.findById(usuario.getIdUsuario());
-	    Optional<Restaurante> restauranteOpt = restauranteDAO.findById(usuario.getIdUsuario());
-	    Optional<Repartidor> repartidorOpt = repartidorDAO.findById(usuario.getIdUsuario());
+		model.addAttribute(USUARIO_STR, usuario);
+		Optional<Cliente> clienteOpt = clienteDAO.findById(usuario.getIdUsuario());
+		Optional<Restaurante> restauranteOpt = restauranteDAO.findById(usuario.getIdUsuario());
+		Optional<Repartidor> repartidorOpt = repartidorDAO.findById(usuario.getIdUsuario());
 
-	    String resultadoCliente = manejarCliente(clienteOpt, usuario, model);
-	    String resultadoRestaurante = manejarRestaurante(restauranteOpt, usuario, model);
-	    String resultadoRepartidor = manejarRepartidor(repartidorOpt, usuario, model);
-	    
-	    if (resultadoCliente != null) { 
-	    	return resultadoCliente;
-	    }else if (resultadoRestaurante != null) { 
-	    	return resultadoRestaurante;
-	    }else if (resultadoRepartidor != null) {
-	    	return resultadoRepartidor;
-	    } else {
-		    model.addAttribute(ERROR_STR, "El usuario no existe, pruebe otra vez");
-	    	return LOGIN_STR;
-	    }
+		String resultadoCliente = manejarCliente(clienteOpt, usuario, model);
+		String resultadoRestaurante = manejarRestaurante(restauranteOpt, usuario, model);
+		String resultadoRepartidor = manejarRepartidor(repartidorOpt, usuario, model);
+
+		if (resultadoCliente != null) {
+			return resultadoCliente;
+		} else if (resultadoRestaurante != null) {
+			return resultadoRestaurante;
+		} else if (resultadoRepartidor != null) {
+			return resultadoRepartidor;
+		} else {
+			model.addAttribute(ERROR_STR, "El usuario no existe, pruebe otra vez");
+			return LOGIN_STR;
+		}
 	}
 
 	@PostMapping("/registro")
@@ -106,29 +106,29 @@ public class GestorUsuario {
 		System.out.println(rol);
 		switch (rol) {
 			case 1:
-			if (clienteDAO.findById(usuario.getIdUsuario()).isPresent()) {
-                model.addAttribute("error", "El nombre de usuario ya existe.");
-                return REGISTRO_STR;
-            }
-				if(registrarCliente(usuario, apellidosCliente, dniCliente, model) == 0)
+				if (clienteDAO.findById(usuario.getIdUsuario()).isPresent()) {
+					model.addAttribute(ERROR_STR, USUARIO_EXISTE_STR);
 					return REGISTRO_STR;
-				
+				}
+				if (registrarCliente(usuario, apellidosCliente, dniCliente, model) == 0)
+					return REGISTRO_STR;
+
 				break;
 			case 2:
-			if (restauranteDAO.findById(usuario.getIdUsuario()).isPresent()) {
-                model.addAttribute("error", "El nombre de usuario ya existe.");
-                return REGISTRO_STR;
-            }
-				if(registrarRestaurante(usuario, codigoPostalRestaurante, calleRestaurante, numeroRestaurante,
+				if (restauranteDAO.findById(usuario.getIdUsuario()).isPresent()) {
+					model.addAttribute(ERROR_STR, USUARIO_EXISTE_STR);
+					return REGISTRO_STR;
+				}
+				if (registrarRestaurante(usuario, codigoPostalRestaurante, calleRestaurante, numeroRestaurante,
 						complementoRestaurante, municipioRestaurante, cifRestaurante, model) == 0)
-						return REGISTRO_STR;
+					return REGISTRO_STR;
 				break;
 			case 3:
-			if (repartidorDAO.findById(usuario.getIdUsuario()).isPresent()) {
-                model.addAttribute("error", "El nombre de usuario ya existe.");
-                return REGISTRO_STR;
-            }
-				if(registrarRepartidor(usuario, apellidosRepartidor, nifRepartidor, model) == 0)
+				if (repartidorDAO.findById(usuario.getIdUsuario()).isPresent()) {
+					model.addAttribute(ERROR_STR, USUARIO_EXISTE_STR);
+					return REGISTRO_STR;
+				}
+				if (registrarRepartidor(usuario, apellidosRepartidor, nifRepartidor, model) == 0)
 					return REGISTRO_STR;
 				break;
 			default:
@@ -156,7 +156,7 @@ public class GestorUsuario {
 			String numeroRestaurante,
 			String complementoRestaurante, String municipioRestaurante, String cifRestaurante,
 			Model model) {
-		if (usuario != null && comprobarCondicionesRegistrarRestaurante(codigoPostalRestaurante, calleRestaurante, 
+		if (usuario != null && comprobarCondicionesRegistrarRestaurante(codigoPostalRestaurante, calleRestaurante,
 				numeroRestaurante, complementoRestaurante, municipioRestaurante, cifRestaurante)) {
 			Direccion dir = new Direccion(codigoPostalRestaurante, calleRestaurante, numeroRestaurante,
 					complementoRestaurante, municipioRestaurante);
@@ -178,70 +178,66 @@ public class GestorUsuario {
 					usuario.getPass(), apellidosRepartidor, nifRepartidor);
 			repartidorDAO.save(repartidor);
 			return 1;
-		}else{
+		} else {
 			model.addAttribute(ERROR_STR, RELLENA_CAMPOS);
 			return 0;
 		}
 
 	}
-	
-	private boolean comprobarCondicionesRegistrarRestaurante(String codigoPostal, String calle, String numero, 
-		String complemento, String municipio, String cif) {
-		
+
+	private boolean comprobarCondicionesRegistrarRestaurante(String codigoPostal, String calle, String numero,
+			String complemento, String municipio, String cif) {
+
 		boolean correcto = true;
-		
+
 		if (codigoPostal.isEmpty() || calle.isEmpty() || numero.isEmpty()) {
 			correcto = false;
 		}
 		if (complemento.isEmpty() || municipio.isEmpty() || cif.isEmpty()) {
-		    correcto = false;
+			correcto = false;
 		}
-		
+
 		return correcto;
-		
+
 	}
-	
-	
+
 	private String manejarCliente(Optional<Cliente> clienteOpt, Usuario usuario, Model model) {
-	    if (clienteOpt.isPresent()) {
-	        Cliente cliente = clienteOpt.get();
-	        if (cliente.getPass().equals(usuario.getPass())) {
-	            return "redirect:/menucliente/" + cliente.getIdUsuario();
-	        } else {
+		if (clienteOpt.isPresent()) {
+			Cliente cliente = clienteOpt.get();
+			if (cliente.getPass().equals(usuario.getPass())) {
+				return "redirect:/menucliente/" + cliente.getIdUsuario();
+			} else {
 				model.addAttribute(ERROR_STR, CONTRASENA_STR);
 				return LOGIN_STR;
 			}
-	    }
-	    return null;
+		}
+		return null;
 	}
 
 	private String manejarRestaurante(Optional<Restaurante> restauranteOpt, Usuario usuario, Model model) {
-	    if (restauranteOpt.isPresent()) {
-	        Restaurante restaurante = restauranteOpt.get();
-	        if (restaurante.getPass().equals(usuario.getPass())) {
-	            return "redirect:/menurestaurante/" + restaurante.getIdUsuario();
-	        } else {
+		if (restauranteOpt.isPresent()) {
+			Restaurante restaurante = restauranteOpt.get();
+			if (restaurante.getPass().equals(usuario.getPass())) {
+				return "redirect:/menurestaurante/" + restaurante.getIdUsuario();
+			} else {
 				model.addAttribute(ERROR_STR, CONTRASENA_STR);
 				return LOGIN_STR;
 			}
-	    }
-	    return null;
+		}
+		return null;
 	}
 
 	private String manejarRepartidor(Optional<Repartidor> repartidorOpt, Usuario usuario, Model model) {
-	    if (repartidorOpt.isPresent()) {
-	        Repartidor repartidor = repartidorOpt.get();
-	        if (repartidor.getPass().equals(usuario.getPass())) {
-	            return "redirect:/menurepartidor/" + repartidor.getIdUsuario();
-	        } else {
+		if (repartidorOpt.isPresent()) {
+			Repartidor repartidor = repartidorOpt.get();
+			if (repartidor.getPass().equals(usuario.getPass())) {
+				return "redirect:/menurepartidor/" + repartidor.getIdUsuario();
+			} else {
 				model.addAttribute(ERROR_STR, CONTRASENA_STR);
 				return LOGIN_STR;
 			}
-	    }
-	    return null;
+		}
+		return null;
 	}
-	
-	
-	
 
 }
